8e70902244a0cf05c89adacce9429ae88be49298
Andrew Webb <andrew@awebb.info>
2019-03-10 22:42:45 +0000

    Added classifiers to setup.py for license, OS, audience, programming language. Loosened requirements to >python3.4.3 and >= numpy1.11.3

--- setup.py
+++ setup.py
@@ -10,10 +10,22 @@ if __name__ == '__main__':
         author_email='andrew@awebb.info',
         packages=['qcircuits'],
         url='http://www.awebb.info/qcircuits/index.html',
-        license='MIT',
+        license='MIT License',
         description='A package for simulating small-scale quantum computing',
         long_description=open('README.rst').read(),
+        python_requires='>3.4.3',
         install_requires=[
             "numpy >= 1.11.3",
+        ],
+        classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
         ],
     )
