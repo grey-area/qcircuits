91edb39b8820fc2f5a6e3e5e3678ca8d4de9387d
Andrew Webb <andrew@awebb.info>
2019-03-11 22:50:10 +0000

    Added another operator composition unit test and a unit test for applying operators to subsets of qubits

--- tests/tests.py
+++ tests/tests.py
@@ -6,6 +6,9 @@ from scipy.stats import unitary_group, dirichlet
 import qcircuits as qc
 
 
+epsilon = 1e-10
+
+
 def max_absolute_difference(T1, T2):
     return np.max(np.abs(T1[:] - T2[:]))
 
@@ -28,11 +31,26 @@ def random_state(d):
     return qc.state.State(amplitudes)
 
 
-class OperatorCompositionTest(unittest.TestCase):
-    def test_operator_composition(self):
-        for test_i in range(10):
+class OperatorCompositionTests(unittest.TestCase):
+
+    def setUp(self):
+        self.num_tests = 10
+        self.ds = []
+        self.U1s = []
+
+        for test_i in range(self.num_tests):
             d = np.random.randint(3, 7)
             U1 = random_unitary_operator(d)
+            self.ds.append(d)
+            self.U1s.append(U1)
+
+    def test_operator_composition_order(self):
+        """
+        For two operators U1 and U2, and state x, we should have
+        (U1(U2))(x) = U1(U2(x))
+        """
+
+        for test_i, (d, U1) in enumerate(zip(self.ds, self.U1s)):
             U2 = random_unitary_operator(d)
             U3 = random_unitary_operator(d)
             x = random_state(d)
@@ -41,9 +59,71 @@ class OperatorCompositionTest(unittest.TestCase):
             result2 = (U1(U2))(U3(x))
             result3 = (U1(U2(U3)))(x)
             max_diff = max_absolute_difference(result1, result2)
-            self.assertTrue(max_diff < 1e-8)
+            self.assertTrue(max_diff < epsilon)
             max_diff = max_absolute_difference(result1, result3)
-            self.assertTrue(max_diff < 1e-8)
+            self.assertTrue(max_diff < epsilon)
+
+    def test_operator_identity_composition(self):
+        """
+        The composition of an operator with the identity operator
+        should be the same as the original operator.
+        """
+
+        for test_i, (d, U) in enumerate(zip(self.ds, self.U1s)):
+            I = qc.Identity(d)
+            R = I(U(I))
+
+            max_diff = max_absolute_difference(U, R)
+            self.assertTrue(max_diff < epsilon)
+
+
+class ApplyingToSubsetsOfQubitsTests(unittest.TestCase):
+
+    def test_application_to_qubit_subset(self):
+        """
+        Instead of applying the tensor product of the Identity
+        operator with another operator to a state, we can apply
+        the operator to a subset of axes of the state. We should
+        get the same result. We can also permute the order of
+        operators in the tensor product, and correspondingly
+        permute the application order.
+        """
+
+        num_tests = 10
+        I = qc.Identity()
+
+        for test_i in range(num_tests):
+            d = np.random.randint(3, 7)
+            num_apply_to = np.random.randint(2, d)
+            apply_to_indices = np.random.choice(d, size=num_apply_to, replace=False)
+
+            M_all = None
+            Ops = []
+            for qubit_i in range(d):
+                if qubit_i in apply_to_indices:
+                    Op = random_unitary_operator(d=1)
+                else:
+                    Op = I
+                Ops.append(Op)
+                if M_all is None:
+                    M_all = Op
+                else:
+                    M_all = M_all * Op
+
+            M_subset = None
+            for apply_to_index in apply_to_indices:
+                Op = Ops[apply_to_index]
+                if M_subset is None:
+                    M_subset = Op
+                else:
+                    M_subset = M_subset * Op
+
+            x = random_state(d)
+
+            result1 = M_all(x)
+            result2 = M_subset(x, qubit_indices=apply_to_indices)
+            max_diff = max_absolute_difference(result1, result2)
+            self.assertTrue(max_diff < epsilon)
 
 
 if __name__ == '__main__':
