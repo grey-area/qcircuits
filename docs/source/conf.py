c240235edfe0fab4a3f948bfd086ca751fe12b4a
Andrew M. Webb <andrew@awebb.info>
2019-04-22 14:05:38 +0100

    Bumped version number, updated changes log.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Andrew Michael Webb'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
